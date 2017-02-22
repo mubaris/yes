a59d66e26c63ddc27d82db767c86303f0ff795a3
The Binary Fox <thebinaryfox@gmail.com>
2017-02-22 11:59:37 -0800

    yes.js: Optimization.

--- yes.js
+++ yes.js
@@ -1,13 +1,15 @@
-if (process.argv.length == 2) {
-    while (1) {
-        console.log("y")
-    }
-} else {
-    output = ""
-    for (i = 2; i < process.argv.length; i++) {
-        output += process.argv[i] + " "
-    }
-    while (1) {
-        console.log(output)
-    }
+// Get output string.
+let output = "y\n";
+if (process.argv.length !== 2) {
+	output = process.argv.slice(2).join(' ') + "\n";
 }
+
+// Write.
+function flood() {
+	while (process.stdout.write(buffer) !== false);
+}
+
+const buffer = output.repeat(Math.floor(4096 / output.length));
+process.stdout.on('drain', flood);
+flood();
+
