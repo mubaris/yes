18199c4566b0ae1770ca5edc9bbb96d7210c8d43
TheBinaryFox <thebinaryfox@gmail.com>
2017-02-22 23:51:35 -0800

    yes.c: Optimization.

--- yes.c
+++ yes.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-    if (argc == 1) {
-        while (1) {
-            printf("y\n");
-        }
-    } else {
-        while (1) {
-            for (int i = 1; i < argc; i++) {
-                printf("%s ", argv[i]);
-            }
-            printf("\n");
-        }
-    }
+#define max(a,b) \
+	({ __typeof__ (a) _a = (a); \
+	 __typeof__ (b) _b = (b); \
+	 _a > _b ? _a : _b; });
+
+
+int main(int argc, char* argv[]) {
+	char* output;
+	int output_size;
+	int output_len;
+
+	if (argc > 1) {
+		// Create buffer.
+		int needed_size = 2 + strlen(argv[1]);
+		for (int i = 2; i < argc; i++) {
+			needed_size += 1 + strlen(argv[i]);
+		}
+
+		output = (char*) malloc(needed_size);
+		
+		// Append to buffer.
+		strcat(output, argv[1]);
+		for (int i = 2; i < argc; i++) {
+			strcat(output, " ");
+			strcat(output, argv[i]);
+		}
+		strcat(output, "\n");
+	} else {
+		output = "y\n";
+		output_len = 2;
+	}
+
+	// Flood.
+	while (1) {
+		fputs(output, stdout);
+	}	
 }
+
