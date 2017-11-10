0cf25dc0a7c8e267a4e415aebdb2e90a69989155
Alex Kir <osleg@users.noreply.github.com>
2017-11-11 03:28:39 +0800

    Added optional argv
    Added optional arg to be on par with original `yes`

--- yes.go
+++ yes.go
@@ -8,13 +8,26 @@ import (
 var bufsize = 64 * 1024
 
 func main() {
-	y := byte('y')
-	n := byte('\n')
-	buf := make([]byte, bufsize)
-	for i := 0; i < len(buf)-2; i += 2 {
-		buf[i] = y
-		buf[i+1] = n
+	var y []byte
+	// Get arg
+	if len(os.Args) > 1 {
+		y = []byte(os.Args[1] + "\n")
+	} else {
+		// Set output to y
+		y = []byte("y\n")
 	}
+	yLen := len(y)
+
+	// Create buffer
+	buf := make([]byte, bufsize)
+	// Popoulate buffer
+	for i := 0; i < len(buf)-yLen; i += yLen {
+		for s := 0; s < len(y); s++ {
+			buf[i+s] = y[s]
+		}
+	}
+
+	// Create buffered writer
 	f := bufio.NewWriterSize(os.Stdout, bufsize)
 	defer f.Flush()
 	for {
