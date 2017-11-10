d7541045d5349986e8507ce7d6383183fcb92777
Alex Kir <osleg@users.noreply.github.com>
2017-11-11 03:00:03 +0800

    Update to speed up yes to be on par with system `yes`
    System `yes` command on my system works very fast
    ```
    $ yes | pv -r > /dev/null 
    [9.06GiB/s]
    ```
    
    Therefore I decided to speed up go version of yes to be on par with system version
    
    ```
    $ ./yes | pv -r > /dev/null
    [9.63GiB/s]
    ```

--- yes.go
+++ yes.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"os"
-	"strings"
 )
 
-func main() {
-	output := "y"
-	if len(os.Args) > 1 {
-		output = strings.Join(os.Args[1:], " ")
-	}
+var bufsize = 64 * 1024
 
+func main() {
+	y := byte('y')
+	n := byte('\n')
+	buf := make([]byte, bufsize)
+	for i := 0; i < len(buf)-2; i += 2 {
+		buf[i] = y
+		buf[i+1] = n
+	}
+	f := bufio.NewWriterSize(os.Stdout, bufsize)
+	defer f.Flush()
 	for {
-		println(output)
+		f.Write(buf)
 	}
 }
